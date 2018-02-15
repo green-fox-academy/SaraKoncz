e785fccf647d2d8f288400fdb7380a28249eb701
SaraKoncz <sara.b.koncz@gmail.com>
2018-02-15 11:37:41 +0100

    With this commint I rename the day folders

--- week-01/day-03/hello-others.ts
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-console.log('Hello, Eszter!');
-console.log('Hello, Szandi!');
-console.log('Hello, Dóri!');
--- week-01/day-03/introduce-yourself.ts
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-console.log('Sara Koncz');
-console.log(24);
-console.log(1.77);
