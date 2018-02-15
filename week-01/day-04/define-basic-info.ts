9165064d58b4c6a45b65baf312c6fd002f44574a
SaraKoncz <sara.b.koncz@gmail.com>
2018-02-15 12:05:56 +0100

    define basic info

---
+++ week-01/day-04/define-basic-info.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let myName: string = 'Sara';
+let myAge: number = 24;
+let myHeight: number = 1.77;
+let myStatus: boolean = false;
+
+console.log('My name: ', myName);
+console.log('My age: ', myAge);
+console.log('My height: ', myHeight);
+console.log('My marriage status: ', myStatus);
+
