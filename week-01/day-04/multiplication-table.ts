c34036f36226509716e326b39c9ad8f3dcbafd97
SaraKoncz <sara.b.koncz@gmail.com>
2018-02-15 15:17:02 +0100

    adding excercises with loops

---
+++ week-01/day-04/multiplication-table.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+let number: number = 15;
+
+for (let multi: number = 1; multi <=10; multi++) {
+    let result: number = multi * number;
+    console.log(`${multi} * ${number} = ${result}`);
+}
+
+// Create a program that prints the multiplication table with number
+// Example:
+// The number 15 should print:
+//
+// 1 * 15 = 15
+// 2 * 15 = 30
+// 3 * 15 = 45
+// 4 * 15 = 60
+// 5 * 15 = 75
+// 6 * 15 = 90
+// 7 * 15 = 105
+// 8 * 15 = 120
+// 9 * 15 = 135
+// 10 * 15 = 150
---
+++ week-01/day-04/print-even.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+let i : number = 0;
+while (i < 488){
+    i += 2;
+    console.log(i);   
+}
