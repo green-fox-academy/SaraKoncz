9acebacb5070ca28e26f43c1bdfd2cb7fff3e334
SaraKoncz <sara.b.koncz@gmail.com>
2018-02-20 12:59:12 +0100

    reversed with for loop

--- week-02/day-02/reverse.ts
+++ week-02/day-02/reverse.ts
@@ -2,8 +2,11 @@
 // Create a function that can reverse a string, which is passed as the parameter
 // Use it on this reversed string to check it!
 let reversed: string = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
-let reversed2: string = '';
+function reverse(s: string){
+    let reversed2: string = '';
 for (let i = reversed.length; i >= 0; i--){
     reversed2 += reversed.charAt(i);
 }
-console.log(reversed2);
+return reversed2;
+}
+console.log(reverse(reversed));
