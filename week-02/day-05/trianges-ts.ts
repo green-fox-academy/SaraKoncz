c3f9ecda57b4c43817eba32f121c6ccbe18597e7
SaraKoncz <sara.b.koncz@gmail.com>
2018-02-24 15:02:00 +0100

    triangles

--- week-02/day-05/trianges-ts.ts
+++ week-02/day-05/trianges-ts.ts
@@ -5,87 +5,41 @@ const ctx = canvas.getContext('2d');
 
 
 // m coefficient (to calculate the height of the triangle)
-let m: number = Math.sqrt(3)/2;
+let m: number = Math.sqrt(3) / 2;
 // I decided to make the length of the triangle's sides half of the canvas width
-let tl: number = canvas.scrollWidth/2
-    ctx.beginPath();
-    ctx.moveTo(0, m*tl);
-    ctx.strokeStyle = 'pink';
-    ctx.lineTo(tl, m*tl);
-    ctx.lineTo(tl/2, 0);
-    ctx.lineTo(0, m*tl);
-    ctx.stroke();
+let tl: number = canvas.scrollWidth;
+//No we a drawing small triangles in it, starting from the left bottom. 
+let myLineNum: number = 68; //How many small triangles you want to start with?
 
-//No we start drawing small triangles in it, first from the top
-let stl: number = tl/21;
-let start: number = 0;
-let stlMoves: number = start + stl;
-let c: number = stl/2;
+function drawTriangle(lineNum: number) {
+    let stl: number = tl / lineNum;         //stl: small triangle length
+    let a: number = 0;
+    let b: number = a + stl;
+    let c: number = stl / 2;
+    let y: number = m * tl;
+    let lineCount: number = lineNum;
 
-for (let i = 1; i <= 4; i++) {
-    ctx.beginPath();
-    ctx.moveTo(start, m*tl);
-    ctx.strokeStyle = 'blue';
-    ctx.lineTo(stlMoves, m*tl);
-    ctx.lineTo(c, m*tl-m*stl);
-    ctx.lineTo(start, m*tl);
-    ctx.stroke();
-    start += stl;
-    c += stl;
-    
+    for (let j = 1; j <= lineNum; j++) {
+
+        for (let i = 1; i <= lineCount; i++) {
+            ctx.beginPath();
+            ctx.moveTo(a, y);
+            ctx.strokeStyle = 'pink';
+            ctx.lineTo(b, y);
+            ctx.lineTo(c, y - m * stl);
+            ctx.lineTo(a, y);
+            ctx.stroke();
+            a += stl;
+            c += stl;
+            b += stl;
+        }
+        lineCount -= 1;
+        a = ((stl / 2) * (lineNum - lineCount));
+        b = ((stl / 2) * (lineNum - lineCount + 2));
+        c = ((stl / 2) * (lineNum - lineCount + 1));
+        y -= (m * stl);
     }
-    
-// for (let i = 1; i <= 21; i++) {
-// ctx.beginPath();
-// ctx.moveTo(start, m*tl);
-// ctx.strokeStyle = 'blue';
-// ctx.lineTo(stlMoves, m*tl);
-// ctx.lineTo(c, m*tl-m*stl);
-// ctx.lineTo(start, m*tl);
-// ctx.stroke();
-// start += stl;
-// c += stl;
-// stlMoves *= 1;
-// }
-
-
-/*
-//I want 14 lines this time so the distance ("a") between the lines I get by dividing the height by 14
-let myLinesCount: number = 14;
-
-function linePlay (height: number, linesCount: number) {
-let a: number = height/linesCount;
-let x1Zero: number = 0;
-let y1: number = 0;
-let x2: number = height;
-let y2Zero: number = 0;
-
-for (let i = 0; i <= linesCount; i++) { 
-    ctx.beginPath();
-    ctx.moveTo(x1Zero, y1);
-    ctx.strokeStyle = 'blue';
-    ctx.lineTo(x2, y2Zero);
-    ctx.stroke();  
-    y1 += a;  
-    x2 -= a;
-}
-// now wee need to set them again to the original value and insted of Zero-s, use Heights
-y1 = 0;
-x2 = height;
-x1Zero = height;
-y2Zero = height;
-
-for (let i = 0; i <= linesCount; i++) {      
-    ctx.beginPath();
-    ctx.moveTo(x1Zero, y1);
-    ctx.strokeStyle = 'blue';    
-    ctx.lineTo(x2, y2Zero);
-    x2 -= a;
-    ctx.stroke();   
-    y1 += a;   
-}
 }
 
-linePlay(myHeight, myLinesCount);
+drawTriangle(myLineNum);
 
-*/
