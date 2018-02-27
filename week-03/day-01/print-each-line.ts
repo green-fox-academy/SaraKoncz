292f492f5622af1c701ca295acb9142c004ca30c
SaraKoncz <sara.b.koncz@gmail.com>
2018-02-27 12:54:11 +0100

    printeachline

---
+++ week-03/day-01/print-each-line.ts
@@ -0,0 +1,22 @@
+// Write a program that opens a file called 'my-file.txt', then prints
+// each of lines form the file.
+// If the program is unable to read the file (for example it does not exists),
+// then it should print an error message like: 'Unable to read file: my-file.txt'
+
+'use strict';
+const fs = require('fs');
+
+
+
+function printLines() {
+    try {
+        let fileContent = fs.readFileSync('my-file.txt', 'utf-8');
+        console.log(fileContent);
+    }
+    catch (e) {
+        console.log('File not found');
+    }
+
+}
+
+printLines();
