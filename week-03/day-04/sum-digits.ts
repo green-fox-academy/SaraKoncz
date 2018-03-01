8d28638d3f219bf922429ae80a62fbccd48f924d
SaraKoncz <sara.b.koncz@gmail.com>
2018-03-01 11:04:39 +0100

    sum-digits

---
+++ week-03/day-04/sum-digits.ts
@@ -0,0 +1,19 @@
+// Given a non-negative int n, return the sum of its digits recursively (no loops). 
+// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+'use strict';
+let divide: number = 10;
+let sum: number = 0;
+let num: number = 12481;
+function sumDigits (num: number): number {  
+  let b: number = Math.floor(num%10);
+  console.log(b);
+  if (num < 1){
+    return 0;
+  }
+  
+  return b += sumDigits((num/10));
+}
+
+console.log(sumDigits(12481));
+
