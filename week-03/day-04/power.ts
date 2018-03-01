d4a74dfa4475245a6c6b71e37a8d52a5ae0492b1
SaraKoncz <sara.b.koncz@gmail.com>
2018-03-01 11:33:38 +0100

    power

---
+++ week-03/day-04/power.ts
@@ -0,0 +1,14 @@
+// Given base and n that are both 1 or more, compute recursively (no loops)
+// the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+'use strict';
+function powerN(base: number, n: number): number {
+  let multi: number = 1;
+  if (n < 1) {
+    return multi;
+  }  
+  multi *= base;
+  n -= 1;
+  return multi *= powerN (base, n);
+}
+
+console.log(powerN(4,3));
