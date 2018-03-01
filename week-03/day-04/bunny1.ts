71ea9ee0bbb0b7ff890d1b41b9c41283109f3132
SaraKoncz <sara.b.koncz@gmail.com>
2018-03-01 17:43:16 +0100

    bunny1 mod

--- week-03/day-04/bunny1.ts
+++ week-03/day-04/bunny1.ts
@@ -2,14 +2,14 @@
 // We want to compute the total number of ears across all the bunnies recursively 
 //(without loops or multiplication).
 'use strict';
-function countFloopyBunnyEars (bunny: number): number {
+function countFloopyBunnyEars(bunny: number): number {
   let bunnyEars: number = 0;
-  if (bunny <1) {
+  if (bunny < 1) {
     return bunnyEars;
   }
   bunnyEars += 2;
   bunny -= 1;
-  return  bunnyEars += countFloopyBunnyEars (bunny);
+  return bunnyEars += countFloopyBunnyEars(bunny);
 }
 
 console.log(countFloopyBunnyEars(4));
