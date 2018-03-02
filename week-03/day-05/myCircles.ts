801f8cd88c1da414a7cce0db7647d88096e4c8e3
SaraKoncz <sara.b.koncz@gmail.com>
2018-03-02 14:22:17 +0100

    myCircles

--- week-03/day-05/myCircles.ts
+++ week-03/day-05/myCircles.ts
@@ -10,7 +10,7 @@ let x: number = radius;
 let y: number = radius;
 
 function circles(x: number, y: number, radius: number) {
-  ctx.lineWidth = 1;
+  ctx.lineWidth = 2;
   ctx.strokeStyle = 'pink';
   ctx.beginPath();
   ctx.arc(x, y, radius, 0 * conv, 360 * conv);
@@ -22,13 +22,12 @@ function circles(x: number, y: number, radius: number) {
   ctx.beginPath();
   ctx.stroke();
   if (radius > 10) {
-    circles(x, y - radius / 2, radius / 2);
-    circles(x + radius / 2 * Math.cos(Math.PI / 6), y + radius / 2 * Math.sin(Math.PI / 6), radius / 2);
-    circles(x - radius / 2 * Math.cos(Math.PI / 6), y + radius / 2 * Math.sin(Math.PI / 6), radius / 2);
+    setTimeout(function () {      
+      circles(x, y - radius / 2, radius / 2);      
+      circles(x + radius / 2 * Math.cos(Math.PI / 6), y + radius / 2 * Math.sin(Math.PI / 6), radius / 2);
+      circles(x - radius / 2 * Math.cos(Math.PI / 6), y + radius / 2 * Math.sin(Math.PI / 6), radius / 2);
+    },500);
   }
 }
 
 circles(x, y, radius);
-
-
-
