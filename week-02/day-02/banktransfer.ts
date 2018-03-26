edda74d8b7021e7a285eb13d3db5fa86f59be705
SaraKoncz <sara.b.koncz@gmail.com>
2018-03-26 11:52:59 +0200

    further leftovers

--- week-02/day-02/banktransfer.ts
+++ week-02/day-02/banktransfer.ts
@@ -1,11 +1,4 @@
 'use strict';
-
-let accounts: any[] = [
-  { client_name: 'Igor', account_number: 11234543, balance: 203004099.2 },
-  { client_name: 'Vladimir', account_number: 43546731, balance: 5204100071.23 },
-  { client_name: 'Sergei', account_number: 23456311, balance: 1353600.0 }
-];
-
 // Create function that returns the name and balance of cash on an account in a list
 // The output should be: "Igor", "203004099.2"
 // Create function that transfers an amount of cash from one account to another
@@ -23,3 +16,44 @@ let accounts: any[] = [
 //	{ 'client_name': 'Vladimir', 'account_number': 43546731, 'balance': 5204099571.23 },
 //	{ 'client_name': 'Sergei', 'account_number': 23456311, 'balance': 1354100.0 }
 //]
+
+'use strict';
+let accounts: any[] = [
+  { client_name: 'Igor', account_number: 11234543, balance: 203004099.2 },
+  { client_name: 'Vladimir', account_number: 43546731, balance: 5204100071.23 },
+  { client_name: 'Sergei', account_number: 23456311, balance: 1353600.0 }
+];
+
+class MyBank {
+  private client_name: string;
+  private account_number: number;
+  private balance: number;
+
+  constructor(client_name, account_number, balance) {
+    this.client_name = client_name;
+    this.account_number = account_number;
+    this.balance = balance;
+  }
+
+  get name(): string {
+    return this.client_name;
+  }
+  get account(): number {
+    return this.account_number;
+  }
+  get money(): number {
+    return this.balance;
+  }
+  set transfer(balance: number) {
+    this.balance = balance;
+  }
+}
+
+let Igor = new MyBank ('Igor', 11234543, 203004099.2)
+let Vladimir = new MyBank ('Vladimir', 43546731, 5204100071.23)
+let Sergei = new MyBank ('Sergei', 23456311, 1353600.0)
+
+
+function balanceCheck(partner: string, accountNumber: number) {
+let num: number = Igor.account
+}
--- week-03/day-03/animal.ts
+++ week-03/day-03/animal.ts
@@ -9,14 +9,15 @@
 class Animal {
   hunger: number = 50;
   thirst: number = 50;
-  eat (): number {
-    return this.hunger--;
+  eat () {
+    this.hunger--;
   }
-  drink (): number {
-    return this.thirst--;
+  drink () {
+    this.thirst--;
   }
-  play (): number {
-    return ((this.hunger++),(this.thirst++));
+  play () {
+    this.hunger++;
+    this.thirst++;
   }
 }
 
@@ -25,3 +26,34 @@ console.log(cica);
 cica.eat;
 
 console.log(cica);
+
+class Animal {
+  private hunger: number;
+  private thurst: number;
+
+  constructor(hunger?: number, thurst?: number) {
+      this.hunger = Math.floor(hunger) || 50;
+      this.thurst = Math.floor(thurst) || 50;
+  }
+  eat () {
+    this.hunger--;
+  }
+  drink(){ 
+    this.thurst--;
+  }
+  play() {
+    this.hunger++;
+    this.thurst++;
+  }
+  get hung() {
+    return this.hunger;
+  }
+  get thur() {
+    return this.thurst;
+  }
+}
+
+let cat = new Animal(50, 30.4);
+
+  cat.drink();
+  console.log(cat.thur);
