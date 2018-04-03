f4215b47d1127586f231ff1fe098b55ded36b19a
SaraKoncz <sara.b.koncz@gmail.com>
2018-04-03 11:52:29 +0200

    first local server created

---
+++ week-07/day-02/myserver/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/api/hello', (req, res) => {
+  console.log(req.query);
+  const name = req.query.name || 'Guest';
+  res.json({
+    message: `Hello ${name}!`,
+  })
+})
+
+const mentors = [
+  { name: 'István', age: 30 },
+  { name: 'Bence', age: 42 },
+  { name: 'Balázs', age: 27 },
+]
+
+const listMentors = (req, res) => {
+  //READ DATA FROM DATABASE
+  res.json(mentors);
+}
+
+app.get('/api/mentors', listMentors);
+
+app.get('/api/mentors/:mentorId', (req, res) => {
+  const mentor = mentors[req.params.mentorId - 1];
+  if (mentor === undefined) {
+    res.status(403);
+    res.json({
+      message: `No mentor with id ${req.params.mentorId}`,
+    })
+  } else {
+    res.json(mentor);
+  }
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT} `);
+});
