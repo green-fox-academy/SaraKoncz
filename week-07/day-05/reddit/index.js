646f50c130ef480c026c65b86e1cd649937d556b
SaraKoncz <sara.b.koncz@gmail.com>
2018-04-06 14:01:18 +0200

    post-posts done but the rest is yet to come

--- week-07/day-05/reddit/index.js
+++ week-07/day-05/reddit/index.js
@@ -4,30 +4,50 @@ const path = require('path');
 const PORT = 8080;
 let mysql = require('mysql');
 
+app.use(express.json());
+
 app.get('/hello', (req, res) => {
   res.json({
     message: "Hello World",
   });
 });
 
-let conn = mysql.createConnection ({
+let conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '12345',
   database: 'reddit',
 });
 
-app.get('/posts', function(req, res) {
-  conn.query('SELECT * FROM posts;', function(err, rows) {
+app.get('/posts', function (req, res) {
+  conn.query('SELECT * FROM posts;', function (err, rows) {
     if (err) {
       console.log(err.toString());
       res.satus(500).send('Database error');
       return;
     }
-    res.send({posts: rows});
+    res.send({ posts: rows });
   });
 });
 
+console.log('kiscica '+ Date.now());
+
+app.post('/posts', function (req, res) {
+  const title = req.body.title;
+  const url = req.body.url;
+  if (title === undefined || url === undefined) {
+    res.json({
+      error: 'Please provide title and url data of the new post!'
+    });
+  } else {
+    const currentTime = Date.now();
+    const newPost = { title, url, timestamp: 123456789, score: 0 };
+    conn.query('INSERT INTO posts SET ?', newPost, (err, rows) => {
+      if (err) throw err;
+      res.send({ posts: rows });
+    });
+  }
+});
 
 
 app.listen(PORT, () => {
