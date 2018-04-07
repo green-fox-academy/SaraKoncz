6340f0f785a40322e250f7fd678c9c677c9f7367
SaraKoncz <sara.b.koncz@gmail.com>
2018-04-07 18:29:52 +0200

    movie selector backend done

--- week-07/day-03/movieSelector.js
+++ week-07/day-03/movieSelector.js
@@ -1,29 +1,56 @@
 'use strict';
 
-document.querySelectorAll('select[name=movies]').forEach(e => {
-  e.disabled = true;
-});
-
 document.addEventListener('DOMContentLoaded', catchChoice);
 
-function catchChoice(){
-  document.querySelector('select[name="categories"]').onchange = checkChoice;  
+function catchChoice() {
+  document.querySelector('select[name="categories"]').onchange = checkChoice;
 };
 
-function checkChoice (e) {
-  console.log(e.target.value);
+function checkChoice(e) {
+  let chosenCategory = e.target.value;
+  if (chosenCategory === 'empty') {
+    document.querySelectorAll('select[name=movies]').forEach(e => {
+      e.disabled = true;
+    });
+  } else {
+    document.querySelectorAll('select[name=movies]').forEach(e => {
+      e.disabled = false;
+    });
+    switch (chosenCategory) {
+      case 'scifi':
+        document.querySelectorAll('.drama').forEach(e => {
+          e.disabled = true;
+        });
+        document.querySelectorAll('.comedy').forEach(e => {
+          e.disabled = true;
+        });
+        document.querySelectorAll('.scifi').forEach(e => {
+          e.disabled = false;
+        });
+        break;
+      case 'drama':
+        document.querySelectorAll('.scifi').forEach(e => {
+          e.disabled = true;
+        });
+        document.querySelectorAll('.comedy').forEach(e => {
+          e.disabled = true;
+        });
+        document.querySelectorAll('.drama').forEach(e => {
+          e.disabled = false;
+        });
+        break;
+      case 'comedy':
+        document.querySelectorAll('.scifi').forEach(e => {
+          e.disabled = true;
+        });
+        document.querySelectorAll('.drama').forEach(e => {
+          e.disabled = true;
+        });
+        document.querySelectorAll('.comedy').forEach(e => {
+          e.disabled = false;
+        });
+        break;
+
+    }
+  }
 }
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   document.querySelector('select[name="categories"]').onchange = changeEventHandler;
-// }, false);
-
-// function changeEventHandler(event) {
-//   // You can use “this” to refer to the selected element.
-//   if (!event.target.value) {
-//     alert('Please Select One');
-//   }
-//   else {
-
-//   }
-// }
