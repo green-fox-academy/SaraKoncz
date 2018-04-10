8b1e7c235d1e3d52aaf44489545b4ae8c50a004f
SaraKoncz <sara.b.koncz@gmail.com>
2018-04-10 23:18:58 +0200

    now only proceed function is missing from the js part

--- week-08/day-02/animalFarm.js
+++ week-08/day-02/animalFarm.js
@@ -9,14 +9,14 @@ let animal = {
   },
   eat: function () {
     this.hunger -= 1;
-  }, 
+  },
   drink: function () {
     this.thirst -= 1;
   },
   play: function () {
     this.hunger += 1;
     this.thirst += 1;
-  } 
+  }
 }
 
 function animalConstructor() {
@@ -27,12 +27,12 @@ function animalConstructor() {
 // The farm has slots which defines the number of free places for animals
 // The farm has list of Animals
 // The farm can breed() which creates a new animal if there's place for it
-// The farm can slaughter() which removes the least hungry animal
+// The farm can adopt() which removes the least hungry animal
 // The farm can print reports about their current state:
 // The farm has 11 living animals we are bankrupt
 // The farm can progress from day to a new day by calling it's progress() method:
 // All animals should have their methods called randomly with 50% chance
-// The farm should call its breed and slaughter method at the end of each day
+// The farm should call its breed and adopt method at the end of each day
 // The farm should print report at the end of each day
 // Print the number of sheeps
 // Print "bankrupt" if no animals left
@@ -43,73 +43,54 @@ let farm = {
   slots: 0,
   listOfAnimals: [],
   status: '',
-  create: function(slots){
+  create: function (slots) {
     this.slots = slots;
-    for (let i = 0; i < slots; i++ ){
+    for (let i = 0; i < slots; i++) {
       this.listOfAnimals.push(animalConstructor());
-      console.log(this.listOfAnimals);
     }
     return farm;
   },
-  breed: function() {
-    if (this.slots >= 1){
+  breed: function () {
+    if (this.slots > this.listOfAnimals.length) {
       this.listOfAnimals.push(animalConstructor());
+      this.slots -= 1;
     }
   },
-  slaughter: function() {
-    let sheepIndexToSlaughter = 0;
+  adopt: function () {
+    let sheepIndexToAdopt = 0;
     this.listOfAnimals.forEach((e, i) => {
-      if (e.hunger > sheepIndexToSlaughter){
-        sheepIndexToSlaughter = i;
+      if (e.hunger > sheepIndexToAdopt) {
+        sheepIndexToAdopt = i;
       }
     });
-    this.listOfAnimals = this.listOfAnimals.splice(sheepIndexToSlaughter, 1);
+    this.listOfAnimals.splice(sheepIndexToAdopt, 1);
+    this.slots++;
   },
-  print: function() {
-    if (this.listOfAnimals.length === 0){
+  print: function () {
+    if (this.listOfAnimals.length === 0) {
       this.status = 'bankrupt';
-    } else if (this.listOfAnimals.length === this.slots){
-      this.status = 'full';
     } else {
-      this.status = 'okay';
+      if (this.listOfAnimals.length === this.slots) {
+        this.status = 'full';
+        console.log('slots:' + this.slots)
+      } else {
+        this.status = 'okay';
+        console.log('slots:' + this.slots)
+      }
     }
     return `The farm has ${this.listOfAnimals.length} animals, we are ${this.status}`
   },
-  progress: function(){
+  progress: function () {
 
   }
 }
 
 const sheepFarm = farm.create(20);
-console.log(sheepFarm.print());
-
-sheepFarm.slaughter();
-sheepFarm.listOfAnimals.forEach (e => {
-  console.log(e);
-});
+console.log(sheepFarm.listOfAnimals.length);
+sheepFarm.adopt();
+console.log(sheepFarm.listOfAnimals.length);
 console.log(sheepFarm.print());
 sheepFarm.breed();
-console.log(sheepFarm.listOfAnimals);
-console.log(sheepFarm.print());
-sheepFarm.slaughter();
-console.log(sheepFarm.print());
-sheepFarm.breed();
-sheepFarm.breed();
-sheepFarm.breed();
-sheepFarm.breed();
-sheepFarm.breed();
-sheepFarm.breed();
-sheepFarm.breed();
-console.log(sheepFarm.print());
-sheepFarm.slaughter();
-sheepFarm.slaughter();
-sheepFarm.slaughter();
-sheepFarm.slaughter();
-console.log(sheepFarm.print());
-
-
-
-
 
 /*
 let signUpButton = document.querySelector('#signUpButton');
