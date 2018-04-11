a692ceeffe6905d3bf9c47b870587ed5271cd305
SaraKoncz <sara.b.koncz@gmail.com>
2018-04-11 13:20:21 +0200

    random giphy done

--- week-08/day-03/giphy/giphy.js
+++ week-08/day-03/giphy/giphy.js
@@ -1,17 +1,16 @@
 let giphy = document.getElementById('giphy');
 var button = document.getElementById('btn');
 
-btn.addEventListener('click', function (){
+btn.addEventListener('click', function () {
     let http = new XMLHttpRequest();
     http.open('GET', `https://api.giphy.com/v1/gifs/trending?api_key=hDDt4rniL2KlZNkA8DoBmt03QPbE5BJ1&limit=25&rating=G`);
-    http.onload = function() {
-        var url = JSON.parse(http.response).data[Math.ceil(Math.random()*25)].images.original.url;
-        renderHTML(url);                
+    http.onload = function () {
+        var url = JSON.parse(http.response).data[Math.ceil(Math.random() * 25)].images.original.url;
+        renderHTML(url);
     };
-        http.send();
+    http.send();
 });
 
-function renderHTML(url){
-            giphy.src = url;
+function renderHTML(url) {
+    giphy.src = url;
 };
-
