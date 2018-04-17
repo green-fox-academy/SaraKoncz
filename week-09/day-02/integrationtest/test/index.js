a1b1e0d9ae495ab917a090cc53122a73022f448d
SaraKoncz <sara.b.koncz@gmail.com>
2018-04-17 13:55:14 +0200

    integrationtet done

---
+++ week-09/day-02/integrationtest/test/index.js
@@ -0,0 +1,32 @@
+'use strict';
+var request = require('supertest');
+var app = require('../server');
+var test = require('tape');
+
+// test('First test!', function (t) {
+//   t.end();
+// }); 
+
+// test('Correct users returned', function (t) {
+//   request(app)
+//     .get('/api/users')
+//     .expect('Content-Type', /json/)
+//     .expect(200)
+//     .end(function (err, res) {
+//       t.end();
+//     });
+// }); 
+
+test('Correct users returned', function (t) {
+  request(app)
+    .get('/api/users')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function (err, res) {
+      var expectedUsers = ['John', 'Betty', 'Hal'];
+
+      t.error(err, 'No error');
+      t.same(res.body, expectedUsers, 'Users as expected');
+      t.end();
+    });
+});
